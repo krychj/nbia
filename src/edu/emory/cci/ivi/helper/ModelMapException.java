7b844509aa14a375376662c0ff079ffb0f9895b5
Jarek Krych <jarek.krych@gmail.com>
2013-05-27 12:28:54 -0600

    Added new edu.emory.cci.ivi.helper package containing new HashmapToCQLQuery.

---
+++ src/edu/emory/cci/ivi/helper/ModelMapException.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package edu.emory.cci.ivi.helper;
+
+/**
+ * @author tpan
+ *
+ */
+public class ModelMapException extends Exception {
+
+
+
+	public ModelMapException() {
+		super();
+	}
+
+	public ModelMapException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public ModelMapException(String arg0) {
+		super(arg0);
+	}
+
+	public ModelMapException(Throwable arg0) {
+		super(arg0);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6677944066859289612L;
+}
